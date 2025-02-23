7c458d602446d1b2baa6de57df6e6c0d729ee4e5
adawgT <nguy1434@msu.edu>
2025-02-23 23:11:01 +0000

    cleaned up the file

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-// Define the Alpine.js component function for generating random names
 function nameGenerator() {
     return {
-      // Array of 10 names
       names: ["aaron", "jason", "jayden", "ethan", "kayla", "kite", "thomas", "sam", "julie", "frank", ],
-      // Variable to store the current random name
       randomName: '',
-      // Function to generate a random index and update the randomName property
       generateRandomName() {
         const index = Math.floor(Math.random() * this.names.length);
         this.randomName = this.names[index];
       },
-      // Function to display an alert with the current random name when the name is clicked
       showAlert() {
         alert(this.randomName);
       }
